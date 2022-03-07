da50136b73af8bfae4c1f818224c54dd623a415f
Lijo <lijo.belardi@icloud.com>
2022-03-07 14:26:13 +0100

    Added django's project.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,10 @@ var words = ['Sviluppatore Blockchain', 'Sviluppatore Full Stack', 'Giornalista'
 var counter = 0;
 var word = document.getElementById('carousel-words');
 setInterval(updated_carousel_words, 2000);
-function updated_carousel_words(){
+function updated_carousel_words() {
     word.innerHTML = words[counter];
     counter++;
-    if (counter >= words.length){
+    if (counter >= words.length) {
         counter = 0;
     }
 }
